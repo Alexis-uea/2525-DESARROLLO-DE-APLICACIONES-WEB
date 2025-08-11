7bdab44a2417c8124c5fef689c67c4562d64df75
Alexis uea <159032726+Alexis-uea@users.noreply.github.com>
2025-08-11 12:56:41 -0500

    App

--- mi_proyecto_flask/app.py
+++ mi_proyecto_flask/app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 
+# Crear la aplicación Flask
 app = Flask(__name__)
 
+# Ruta principal
 @app.route('/')
 def inicio():
     return '¡Hola! Esta es la página principal.'
 
+# Ruta dinámica para mostrar mensaje personalizado
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
     return f'¡Hola, {nombre}! Bienvenido a la página.'
 
 if __name__ == '__main__':
+    
+     # Ejecuta la aplicación en el puerto 5000
     app.run(debug=True)
