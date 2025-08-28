3371e32df0b1bd548b7b5a45f4df204cdd3a8a47
Alexis uea <159032726+Alexis-uea@users.noreply.github.com>
2025-08-27 19:59:20 -0500

    Formulario

--- mi_proyecto_flask/forms.py
+++ mi_proyecto_flask/forms.py
@@ -1,8 +1,8 @@
-from flask_wtf import FlaskForm  # Clase base para formularios en Flask-WTF
+from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, DecimalField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
 
-# Formulario para agregar un producto
+# Formulario para agregar producto
 class ProductoForm(FlaskForm):
     codigo = StringField('Código', validators=[DataRequired()])
     nombre = StringField('Nombre', validators=[DataRequired()])
@@ -10,7 +10,7 @@ class ProductoForm(FlaskForm):
     precio = DecimalField('Precio', places=2, validators=[DataRequired(), NumberRange(min=0)])
     submit = SubmitField('Agregar')
 
-# Formulario para editar un producto (sin campo código)
+# Formulario para editar (sin código)
 class EditarProductoForm(FlaskForm):
     nombre = StringField('Nombre', validators=[DataRequired()])
     cantidad = IntegerField('Cantidad', validators=[DataRequired(), NumberRange(min=1)])
