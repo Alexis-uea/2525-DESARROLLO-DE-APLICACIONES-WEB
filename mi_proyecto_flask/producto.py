f4bf5db92ffd304dad106e2812414c5a8e57548e
Alexis uea <159032726+Alexis-uea@users.noreply.github.com>
2025-08-27 19:59:46 -0500

    Productos

--- mi_proyecto_flask/producto.py
+++ mi_proyecto_flask/producto.py
@@ -1,7 +1,7 @@
-# Clase que representa un producto del inventario
+# Clase que representa un producto
 class Producto:
     def __init__(self, codigo, nombre, cantidad, precio):
         self.codigo = codigo        # Código único
         self.nombre = nombre        # Nombre del producto
-        self.cantidad = cantidad    # Unidades disponibles
-        self.precio = precio        # Precio por unidad
+        self.cantidad = cantidad    # Cantidad disponible
+        self.precio = precio        # Precio unitario
